b1caf7c508d980a4e082cb2a51eb618ebc25e71c
Caleb Cox <calebcox2010@gmail.com>
2021-06-22 22:44:49 -0500

    Add Unsplash API variables

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("testing");
+// Unsplash API
+const count = 10;
+const apiKey = `AqkgtIlClRqkUvkQfN00EKZKm94kmqCxT3ANGZP0-uc`;
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
