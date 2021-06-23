f7de30f82e3fbc899e6307d5ce1c24b51b30dcdf
Caleb Cox <calebcox2010@gmail.com>
2021-06-22 22:51:34 -0500

    Add fetch data function

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 const count = 10;
 const apiKey = `AqkgtIlClRqkUvkQfN00EKZKm94kmqCxT3ANGZP0-uc`;
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get Photos
+async function getPhotos() {
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        console.log(data);
+
+    } catch (error) {
+        // Catch Errors Here
+    }
+}
+
+// On Load
+getPhotos();
